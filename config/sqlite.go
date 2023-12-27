67933013acddf8a408cb342081fd584a56bf0db4
Fabio Muniz <fabio.muniz@outlook.com>
2023-12-26 23:11:12 -0300

    feat: add database handler

---
+++ config/sqlite.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+
+	"github.com/fabiomzs/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	//Check database exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("dabase file not fount, creating...")
+
+		//Create database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	//Create database and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	//Migrate
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
+	//Return database
+	return db, nil
+}
