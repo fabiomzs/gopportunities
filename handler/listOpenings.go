5d468a0d78372d5835194244ff3011e052772b63
Fabio Muniz <fabio.muniz@outlook.com>
2023-12-27 13:43:09 -0300

    docs: add documentations

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePah /api/v1
+
+// @Summary List opening
+// @Description List all jobs openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
