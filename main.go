d6086678fa622f210af1fcb800113a80ed6ceec6
Fabio Muniz <fabio.muniz@outlook.com>
2023-12-26 22:50:38 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/fabiomzs/gopportunities/router"
+import (
+	"github.com/fabiomzs/gopportunities/config"
+	"github.com/fabiomzs/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	//Init configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
+
+	// Init router
 	router.Initialize()
 }
